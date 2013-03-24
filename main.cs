8f530f8a5116bf92ea5b6635791134b8330b1de4
Skelon <owenosagie@ymail.com>
2013-03-24 15:37:08 +0000

    still a working progress....

--- main.cs
+++ main.cs
@@ -14,16 +14,7 @@ namespace ChatBot
     {
         public byte[] psuedo;
         public byte[] mdp;
-        public HttpClient socket = new HttpClient()
-        {
-            socket.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
-            socket.DefaultRequestHeaders.Add("Content-Type", content);
-            socket.DefaultRequestHeaders.Add("Content-Length", content);
-            socket.MaxResponseContentBufferSize = 256000;
-            socket.BaseAddress = "https://www.facebook.com/";
-        }
-        
-        
+        public HttpClient socket = new HttpClient(); 
     }
     
     public class FacebookSocket 
@@ -40,20 +31,31 @@ namespace ChatBot
                         Console.WriteLine("Username must be more than 5 characters. / Pseudo doit etre plus que 5 caractres.");
                     }
                 case "chat"
-                {}
+                {
+                    Console.Write("Enter the name of a person you want to chat with / Donnez le nom d\'une person vous voulez parler avec:");
+                    nom = Console.Read();
+                }
                 }
             }
             mdp = args[2];
-            action = args[3];
+
             if (action.Length <= 0)
             {
                 Console.WriteLine(@"Action must be 'connect', 'chat', 'poke', 'pokewar', 'add_friend' or 'upload' / Il faut donner le paramètre action comme ")
             }
             else
-            {if action = }
+            {
+                switch action
+                {
+                    case ("add_friend" || "ajouter_ami")
+                    {
+                        Console.Write("Enter the name of the friend you want to add / Donnez le nom de votre ami vous voulez ajouter:");
+                        ami = Console.Read();
+                    }
+                }
         }
         
-        public static void envoyer(string[] content)
+        public static void envoyer(string[] args)
         {
             socket.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)");
             socket.DefaultRequestHeaders.Add("Content-Type", content);
@@ -61,15 +63,5 @@ namespace ChatBot
             socket.MaxResponseContentBufferSize = 256000;
             socket.BaseAddress = "https://www.facebook.com/";
         }
-        
-        /*public static void envoyer_msg(string[] psuedo, string[] msg)
-        {
-            
-        }*/
-        
-        /*public static void connecte()
-        {
-            
-        }*/
     } 
 }
