1bb62739cbb4de964f811502c046da06e537fef7
Skelon <owenosagie@ymail.com>
2013-03-24 15:51:53 +0000

    Update main.cs

--- main.cs
+++ main.cs
@@ -10,50 +10,56 @@ namespace ChatBot
     //public static bool langage;
     // 0 français
     // 1 english
-    struct Bot 
-    {
-        public byte[] psuedo;
-        public byte[] mdp;
-        public HttpClient socket = new HttpClient(); 
-    }
-    
+    if (action.Length <= 0)
+            {
+                Console.WriteLine(@"Action must be 'connect', 'chat', 'poke', 'pokewar', 'add_friend' or 'upload' / Il faut donner le paramètre action comme ")
+            }
     public class FacebookSocket 
     {   
+        public HttpClient socket = new HttpClient();
+        
         public static void Main(string[] args)
-        { 
-            switch args[0]
+        {
+            if (args.Length = 0)
             {
-                case "login"
-                {
-                    Console.WriteLine("Connexion...")
-                    if args[1].Length <= 0
-                    {
-                        Console.WriteLine("Username must be more than 5 characters. / Pseudo doit etre plus que 5 caractres.");
-                    }
-                case "chat"
-                {
-                    Console.Write("Enter the name of a person you want to chat with / Donnez le nom d\'une person vous voulez parler avec:");
-                    nom = Console.Read();
-                }
-                }
+                Console.WriteLine("")
             }
+            
+            else
+            {
+                switch args[0]
+                {
+                    case "login"
+                    {
+                        Console.WriteLine("Connexion...");
+                        if args[1].Length <= 0
+                        {
+                            Console.WriteLine("Username must be more than 5 characters. / Pseudo doit etre plus que 5 caractres.");
+                        }
+                    }
+                            
+                    case "chat"
+                    {
+                        Console.Write("Enter the name of a person you want to chat with / Donnez le nom d\'une person vous voulez parler avec:");
+                        nom = Console.Read();
+                    }
+                
+                    case ("add_friend" || "ajouter_ami")
+                    {
+                        Console.Write("Enter the name of the friend you want to add / Donnez le nom de votre ami vous voulez ajouter:");
+                        ami = Console.Read();
+                    }
+            }
+        }
+            
+                
             mdp = args[2];
 
             if (action.Length <= 0)
             {
                 Console.WriteLine(@"Action must be 'connect', 'chat', 'poke', 'pokewar', 'add_friend' or 'upload' / Il faut donner le paramètre action comme ")
             }
-            else
-            {
-                switch action
-                {
-                    case ("add_friend" || "ajouter_ami")
-                    {
-                        Console.Write("Enter the name of the friend you want to add / Donnez le nom de votre ami vous voulez ajouter:");
-                        ami = Console.Read();
-                    }
-                }
-        }
+
         
         public static void envoyer(string[] args)
         {
